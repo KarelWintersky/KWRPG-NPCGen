9c4e362170a70a910e40f1e0bcd54539b5d4b81e
KarelWintersky <karel.wintersky@gmail.com>
2014-10-06 09:49:45 +0400

    minor fixes (styling, formatting, inreased number of NPCs)

--- index.php
+++ index.php
@@ -3,13 +3,14 @@ require_once 'core/npc.php';
 require_once 'core/npc.view.php';
 
 $flag_debug = 1;
+$npc_count = 40;
 
 $main_html = new kwt('/templates/npc.list.tpl.html');
 
-$any = new NPC();
-$npc = $any->getNPC();
-
-$npc_row = npcView::formatRow(1, $npc );
+for ($i = 1; $i <= 40; $i++) {
+    $any = new NPC();
+    $npc_row .= npcView::formatRow($i, $any->getNPC());
+}
 
 $main_html->override(array(
     '____npc_list'  => $npc_row
@@ -17,10 +18,4 @@ $main_html->override(array(
 
 $main_html->flush();
 
-
 ?>
-
-<hr>
-<pre>
-    <?php if ($flag_debug) print_r( flatten($npc) ); ?>
-</pre>
