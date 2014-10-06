73e3d8526527f1a88bfcb1804614215cd2bdecc3
KarelWintersky <karel.wintersky@gmail.com>
2014-10-06 09:12:44 +0400

    KWT 1.3 обрабатывает вложенные массивы в override-вызове, превращая ключи в последовательности xxx/yyy/zzz (пока что символ-разделитель задан жёстко и не задается при инициализации шаблона)

--- core/core.kwt.php
+++ core/core.kwt.php
@@ -1,7 +1,28 @@
 <?php
 /*
-KarelWintersky's Template simple engine ver 1.2
+KarelWintersky's Template simple engine ver 1.3
+1.3 changed:
+
+попытка сделать возможность парсить вложенные override-переменные, например
+в override-массив попадает конструкция array ('val1' => 'data1', 'val2' => array( 'val3' => 'n') ) )
+мы хотим такую конструкцию корректно распарсить так, чтобы можно было подставить n по соотв ключу,
+например 'val2/val3' as 'n' или как-то похоже. Внимание, вопрос - как лучше строить такой ключ?
+
 */
+
+function flatten($array, $prefix = '', $suffix = '/')
+{
+    $result = array();
+    foreach ($array as $key => $value)
+    {
+        if (is_array($value))
+            $result = array_merge($result, flatten($value, $prefix . $key . $suffix, $suffix));
+        else
+            $result[$prefix . $key] = $value;
+    }
+    return $result;
+}
+
 
 class kwt
 {
@@ -19,6 +40,20 @@ class kwt
             return ob_get_clean();
         }
         return null; // was false
+    }
+
+    private function buildKey($array, $key)
+    {
+        $value = $array [ $key ];
+        $rkey = '';
+        if (!is_array($value) ) {
+            $rkey = $key;
+        } else {
+            foreach ( $value as $kkey => $kvalue ) {
+                $rkey .= $key.'/'.$this->buildKey( $kvalue, $kkey );
+            }
+        }
+        return $rkey;
     }
 
     // функция-обработчик. заменяет переменные в файле согласно массиву overrides
@@ -48,11 +83,14 @@ class kwt
     {
         if (!empty($arr)) {
             foreach ($arr as $ki => $kv) {
-                if (!array_key_exists(strtolower($ki), $this->overrides)) $this->overrides[strtolower($ki)] = $kv;
+                if (!array_key_exists(strtolower($ki), $this->overrides))
+                    $this->overrides[strtolower($ki)] = $kv;
             }
         } else {
-            $this->overrides = array_merge($this->overrides,$arr);
+            $this->overrides = array_merge($this->overrides, $arr);
         }
+        /* вообще-то надо обрабатывать массив вложенных ключей тут */
+        $this->overrides = flatten( $this->overrides );
     }
 
     // возвращает обработанный шаблон в переменную (для использования в шаблонах верхнего уровня)
